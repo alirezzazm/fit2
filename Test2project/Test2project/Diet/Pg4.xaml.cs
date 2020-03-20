3a596babd3af8bf5ccd6d01ce6db88356179dfe4
alireza <alireza@DESKTOP-90COGFS>
2020-03-20 11:38:47 +0330

    1399/1/1

--- Test2project/Test2project/Diet/Pg4.xaml.cs
+++ Test2project/Test2project/Diet/Pg4.xaml.cs
@@ -16,10 +16,84 @@ namespace Test2project.Diet
         {
             InitializeComponent();
         }
-
+       
         private void TapGestureRecognizer_TappedforDietPg4(object sender, EventArgs e)
         {
             Navigation.PushModalAsync(new Pg3());
         }
+ 
+        private bool isOpen = false;
+
+        private async void mavad(object sender, EventArgs e)
+        {
+            if (isOpen == false)
+            {
+                isOpen = true;
+                //Scale to smaller  
+                await ((Grid)sender).ScaleTo(0.1, 50, Easing.SinIn);
+                //Wait a moment  
+                await Task.Delay(0);
+                //Scale to normal  
+                await ((Grid)sender).ScaleTo(1, 50, Easing.SinIn);
+
+                Bt2.IsVisible = false;
+
+            }
+            else
+            {
+
+
+                isOpen = false;
+                //await scar
+                //Scale to smaller  
+                await ((Grid)sender).ScaleTo(0.1, 50, Easing.Linear);
+                //Wait a moment  
+                await Task.Delay(0);
+                //Scale to normal  
+                await ((Grid)sender).ScaleTo(1, 50, Easing.Linear);
+
+                Bt2.IsVisible = true;
+
+
+
+
+            }
+        }
+        private async void khavas(object sender, EventArgs e)
+        {
+            if (isOpen == false)
+            {
+                isOpen = true;
+                //Scale to smaller  
+                await ((Grid)sender).ScaleTo(0.1, 50, Easing.SinIn);
+                //Wait a moment  
+                await Task.Delay(0);
+                //Scale to normal  
+                await ((Grid)sender).ScaleTo(1, 50, Easing.SinIn);
+
+                Bt1.IsVisible = false;
+
+            }
+            else
+            {
+
+
+                isOpen = false;
+                //await scar
+                //Scale to smaller  
+                await ((Grid)sender).ScaleTo(0.1, 50, Easing.Linear);
+                //Wait a moment  
+                await Task.Delay(0);
+                //Scale to normal  
+                await ((Grid)sender).ScaleTo(1, 50, Easing.Linear);
+
+                Bt1.IsVisible = true;
+
+
+
+
+            }
+        }
+
     }
 }
